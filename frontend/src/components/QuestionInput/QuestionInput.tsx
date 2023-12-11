a4d0c63f8dc8fc0f45ca37693568973f2df9cdb9
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2023-12-11 15:49:40 +0000

    remove suggestions

--- frontend/src/components/QuestionInput/QuestionInput.tsx
+++ frontend/src/components/QuestionInput/QuestionInput.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Stack, TextField } from "@fluentui/react";
 import { SendRegular } from "@fluentui/react-icons";
 import Send from "../../assets/Send.svg";
-import Suggestions from "../..assets/Suggestions.svg"
+// import Suggestions from "../..assets/Suggestions.svg"
 import styles from "./QuestionInput.module.css";
 
 interface Props {
@@ -64,7 +64,6 @@ export const QuestionInput = ({ onSend, disabled, placeholder, clearOnSend, conv
                 onClick={sendQuestion}
                 onKeyDown={e => e.key === "Enter" || e.key === " " ? sendQuestion() : null}
             >
-                <img src={Suggestions} className={styles.questionInputSendButton}/>
                 { sendQuestionDisabled ? 
                     <SendRegular className={styles.questionInputSendButtonDisabled}/>
                     :
