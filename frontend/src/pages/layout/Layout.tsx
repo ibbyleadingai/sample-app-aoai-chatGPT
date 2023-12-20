a1d070bf733376cf6087bda2124601f6bbf9da0d
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2023-12-20 09:29:46 +0000

    Commented out the env variable
    Need to test whether cosmo db is configured

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -38,7 +38,8 @@ const Layout = () => {
     const [copyClicked, setCopyClicked] = useState<boolean>(false);
     const [copyText, setCopyText] = useState<string>("Copy URL");
     const appStateContext = useContext(AppStateContext)
-    const AZURE_HISTORY_VISIBILITY = import.meta.env.VITE_AZURE_HISTORY_VISIBILITY === 'true';
+    // const AZURE_HISTORY_VISIBILITY = import.meta.env.VITE_AZURE_HISTORY_VISIBILITY === 'true';
+    // console.log(appStateContext?.state.isCosmosDBAvailable?.status)
 
 
     const handleShareClick = () => {
@@ -85,12 +86,12 @@ const Layout = () => {
                         </Link>
                     </Stack>
                     <Stack horizontal tokens={{ childrenGap: 4 }}>
-                            {AZURE_HISTORY_VISIBILITY && 
+                            {/* {AZURE_HISTORY_VISIBILITY && 
                                 <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? "Hide chat history" : "Show chat history"}/>    
-                            }
-                            {/* {(appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured) && 
+                            } */}
+                            {(appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured) && 
                                 <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? "Hide chat history" : "Show chat history"}/>    
-                            } Original conditional render of history btn*/}
+                            } Original conditional render of history btn
                             <ShareButton onClick={handleShareClick} />
                     </Stack>
 
