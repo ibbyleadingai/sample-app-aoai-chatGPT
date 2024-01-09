fea0b6f625467caaa89249663a8da5110c904878
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-01-09 10:31:06 +0000

    code cleanup

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -38,21 +38,6 @@ const Layout = () => {
     const [copyClicked, setCopyClicked] = useState<boolean>(false);
     const [copyText, setCopyText] = useState<string>("Copy URL");
     const appStateContext = useContext(AppStateContext)
-    const [isHistoryVisible, setIsHistoryVisible] = useState<boolean>(true);
-
-    useEffect(() => {
-        const fetchVisibilityConfig = async () => {
-          try {
-            const response = await fetch('/get-visibility-config');
-            const data = await response.json();
-            setIsHistoryVisible(data.azure_history_visible === 'true');
-          } catch (error) {
-            console.error('Error fetching visibility config:', error);
-          }
-        };
-    
-        fetchVisibilityConfig();
-      }, []);
 
     const handleShareClick = () => {
         setIsSharePanelOpen(true);
@@ -98,9 +83,6 @@ const Layout = () => {
                         </Link>
                     </Stack>
                     <Stack horizontal tokens={{ childrenGap: 4 }}>
-                            {/* {isHistoryVisible && 
-                                <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? "Hide chat history" : "Show chat history"}/>    
-                            } */}
                             {(appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured) && 
                                 <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? "Hide chat history" : "Show chat history"}/>    
                             }
