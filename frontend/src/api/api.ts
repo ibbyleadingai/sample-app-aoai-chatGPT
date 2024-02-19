4d01160beb9ae2c389d77ba491763d69175a42a7
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-02-19 15:13:09 +0000

    Debugging env var

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -20,7 +20,7 @@ export const frontendSettings = async (): Promise<Response | null> => {
     const response = await fetch("/frontend_settings", {
         method: "GET",
     }).then((res) => {
-        console.log(res)
+        console.log(res.json())
         return res.json()
     }).catch((err) => {
         console.error("There was an issue fetching your data.");
