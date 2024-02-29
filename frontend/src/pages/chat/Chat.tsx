78810d9a08362d0d5e089dfe161543917f313b7f
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-02-29 16:03:00 +0000

    Upload fea logo

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -12,6 +12,7 @@ import DOMPurify from 'dompurify';
 import styles from "./Chat.module.css";
 // import Contoso from "../../assets/Contoso.svg";
 import LeadingAI from "../../assets/leadingai.svg"
+import fea from "../../assets/fea.svg"
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
@@ -642,7 +643,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={ui?.chat_logo ? ui.chat_logo : LeadingAI}
+                                    src={ui?.chat_logo ? ui.chat_logo : fea}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
