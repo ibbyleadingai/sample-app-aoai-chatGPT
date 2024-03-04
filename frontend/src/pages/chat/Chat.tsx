7ab61296c2b0e3d7c39b8f2386480db9b559754b
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-03-04 13:41:20 +0000

    Update how we call logo env variable

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -643,7 +643,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={ui?.chat_logo ? ui.chat_logo : fea}
+                                    src={`../../assets/${ui?.chat_logo ? ui.chat_logo : fea}`}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
