71bbc0d47231d461efb079be391f1ebc0fca27e3
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-03-06 10:34:40 +0000

    Revert "Testing to see if static src img logo is working"
    This reverts commit 4f789c9da817f4b8d960a9d5a2782883ce6187c2.

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -646,7 +646,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src='../../assets/leadingai.svg'
+                                    src={ui?.chat_logo ? ui.chat_logo : fea}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
