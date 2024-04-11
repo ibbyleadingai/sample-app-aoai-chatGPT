250d847e6ada83cec6d9d414c1cca7b886083fba
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-04-11 10:33:12 +0100

    Added in Coram logo

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -15,6 +15,7 @@ import leadingai from "../../assets/leadingai.svg"
 import mcgill from "../../assets/mcgill&partners.svg"
 import fea from "../../assets/fea.svg"
 import ambition from "../../assets/ambition.png"
+import coram from "../../assets/coram.gif"
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
@@ -47,8 +48,9 @@ type ImageImports = {
     leadingai: leadingai,
     fea: fea,
     mcgill: mcgill,
-    ambition: ambition
-    // Add more entries as needed for other images
+    ambition: ambition,
+    coram: coram
+    // Note: Add more entries as needed for other images
   };
 
 const enum messageStatus {
