9ebbc36ee517fe775266d4845263e15e0459fe46
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-04-24 16:17:02 +0100

    Added a space after Beta

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -100,7 +100,7 @@ const Layout = () => {
                             aria-hidden="true"
                         />}
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle} style={{color: ui?.title_text_color}}>{ui?.title} <i>Beta</i></h1>
+                            <h1 className={styles.headerTitle} style={{color: ui?.title_text_color}}>{ui?.title} <i> Beta</i></h1>
                         </Link>
                     </Stack>
                         <Stack horizontal tokens={{ childrenGap: 4 }}>
