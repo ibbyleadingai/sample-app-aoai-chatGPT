bb6efc43a9847dd48081de3783d47ed2bb55affc
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-04-24 16:43:35 +0100

    Added a space after the UI_TITLE

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -100,7 +100,9 @@ const Layout = () => {
                             aria-hidden="true"
                         />}
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle} style={{color: ui?.title_text_color}}>{ui?.title} <i> Beta</i></h1>
+                            <h1 className={styles.headerTitle} style={{color: ui?.title_text_color}}>
+                            {ui?.title}{' '}<i>Beta</i>
+                            </h1>
                         </Link>
                     </Stack>
                         <Stack horizontal tokens={{ childrenGap: 4 }}>
