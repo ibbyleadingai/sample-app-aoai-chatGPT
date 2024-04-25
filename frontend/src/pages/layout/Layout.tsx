d02f4608f79f9ce529b6c64f4bc9fcef17f8cdad
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-04-25 10:46:59 +0100

    Show/hide history env variable. Change width of improve prompt button

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -96,7 +96,7 @@ const Layout = () => {
                         </Link>
                     </Stack>
                         <Stack horizontal tokens={{ childrenGap: 4 }}>
-                            {(appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured) &&
+                            {(appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured && ui?.show_history_button) &&
                                 <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? hideHistoryLabel : showHistoryLabel} />
                             }
                             {ui?.show_share_button && <ShareButton onClick={handleShareClick} text={shareLabel} />}
