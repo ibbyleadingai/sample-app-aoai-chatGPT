e106e24f21067509a1285d6bb9e01e362eabeb33
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-04-29 11:54:01 +0100

    Hide engine logo image

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -86,11 +86,11 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <Stack horizontal verticalAlign="center" horizontalAlign="space-between">
                     <Stack horizontal verticalAlign="center">
-                        <img
+                        {/* <img
                             src={engineLogo}
                             className={styles.headerIcon}
                             aria-hidden="true"
-                        />
+                        /> */}
                         <Link to="/" className={styles.headerTitleContainer}>
                             <h1 className={styles.headerTitle} style={{color: ui?.title_text_color}}>{ui?.title}</h1>
                         </Link>
