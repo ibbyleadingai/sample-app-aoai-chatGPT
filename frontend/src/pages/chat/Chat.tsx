818cee3ddb56e7c1b3ffa5966a697335c99b8490
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-06-21 10:17:32 +0100

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -793,11 +793,11 @@ const Chat = () => {
                 {/* <img src={ui?.chat_logo ? ui.chat_logo : Contoso} className={styles.chatIcon} aria-hidden="true" /> */}
                 <h1 className={styles.chatEmptyStateTitle} style={{color: ui?.chat_text_color, fontFamily: ui?.chat_font_empty_state, textShadow: ui?.chat_text_shadow ? '1px 1px 4px rgba(0, 0, 0, 0.4)' : 'none'}}>{ui?.chat_title}</h1>
                 <h2 className={styles.chatEmptyStateSubtitle} style={{color: ui?.chat_text_color, fontFamily: ui?.chat_font_empty_state, textShadow: ui?.chat_text_shadow ? '1px 1px 4px rgba(0, 0, 0, 0.4)' : 'none'}}>{ui?.chat_description}</h2>
-                <div className={styles.promptSuggestionsContainer}>
+                {ui?.show_prompt_suggestions && <div className={styles.promptSuggestionsContainer}>
                     <div onClick={() => makeApiRequestWithoutCosmosDB(promptBtnObj.prompt1)} className={styles.promptSuggestions}><h3 className={styles.promptTitle}>{ui?.prompt1_suggestion_text}</h3></div>
                     <div onClick={() => makeApiRequestWithoutCosmosDB(promptBtnObj.prompt2)} className={styles.promptSuggestions}><h3 className={styles.promptTitle}>{ui?.prompt2_suggestion_text}</h3></div>
                     <div onClick={() => makeApiRequestWithoutCosmosDB(promptBtnObj.prompt3)} className={styles.promptSuggestions}><h3 className={styles.promptTitle}>{ui?.prompt3_suggestion_text}</h3></div>
-                </div>
+                </div>}
               </Stack>
             ) : (
               <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? '40px' : '0px' }} role="log">
