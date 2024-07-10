0b8a1961c6a378af2cb6bca92c1dd6183a50379e
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-07-10 13:57:43 +0100

    Added in North Yorkshire Logo

--- frontend/src/imageImports.ts
+++ frontend/src/imageImports.ts
@@ -1,31 +1,33 @@
 import ambition from "./assets/ambition.png"
-import leadingai from "./assets/leadingai.svg"
+import leadingAi from "./assets/leadingai.svg"
 import engine from "./assets/Engine-Mark.svg"
 import morley from "./assets/morley.png"
 import engineLogo from "./assets/Engine-Logo.svg"
 import fea from "./assets/fea.svg"
 import coram from "./assets/coram.gif"
-import glasgowuni from "./assets/glasgowuni.svg"
+import glasgowUni from "./assets/glasgowuni.svg"
 import tri from "./assets/tri.png"
 import nhbc from "./assets/nhbc-logo.svg"
 import nhbcBackgroundImage from "./assets/nhbcimg1.jpg"
+import northYorkshire from "./assets/NorthYorkshire.svg"
 
 type ImageImports = {
     [key: string]: string;
   };
 
   const imageImports: ImageImports = {
-    leadingai: leadingai,
+    leadingAi: leadingAi,
     ambition: ambition,
     engine: engine,
     engineLogo: engineLogo,
     morley: morley,
     coram: coram,
-    glasgowuni: glasgowuni,
+    glasgowUni: glasgowUni,
     fea: fea,
     tri: tri,
     nhbc: nhbc,
-    nhbcBackgroundImage: nhbcBackgroundImage
+    nhbcBackgroundImage: nhbcBackgroundImage,
+    northYorkshire: northYorkshire
     // Add more entries as needed for other images
   };
 
