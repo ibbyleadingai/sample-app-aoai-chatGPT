246f3b3fb4a798fe1eb53beb69cc5a86612e21b1
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-08-19 14:05:19 +0100

    Removed placeholder text on prompt buttons. Text can be empty now

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -750,50 +750,50 @@ const Chat = () => {
 
     const prompts = [
       {
-        headerText: ui?.prompt1_header_text || 'Default Header 1',
-        suggestionText: ui?.prompt1_suggestion_text || 'Default Suggestion 1',
+        headerText: ui?.prompt1_header_text || '',
+        suggestionText: ui?.prompt1_suggestion_text || '',
         message: ui?.prompt1_suggestion_message || 'Hello (prompt 1)',
         imageIcon: imageImports.policyAdvisorIcon
       },
       {
-        headerText: ui?.prompt2_header_text || 'Default Header 2',
-        suggestionText: ui?.prompt2_suggestion_text || 'Default Suggestion 2',
+        headerText: ui?.prompt2_header_text || '',
+        suggestionText: ui?.prompt2_suggestion_text || '',
         message: ui?.prompt2_suggestion_message || 'What can you do? (prompt 2)',
         imageIcon: imageImports.commsProfessional
       },
       {
-        headerText: ui?.prompt3_header_text || 'Default Header 3',
-        suggestionText: ui?.prompt3_suggestion_text || 'Default Suggestion 3',
+        headerText: ui?.prompt3_header_text || '',
+        suggestionText: ui?.prompt3_suggestion_text || '',
         message: ui?.prompt3_suggestion_message || 'What is RAG? (Prompt 3)',
         imageIcon: imageImports.schoolImprovementPlanIcon
       },
       {
-        headerText: ui?.prompt4_header_text || 'Default Header 4',
-        suggestionText: ui?.prompt4_suggestion_text || 'Default Suggestion 4',
+        headerText: ui?.prompt4_header_text || '',
+        suggestionText: ui?.prompt4_suggestion_text || '',
         message: ui?.prompt4_suggestion_message || 'Tell me a joke (prompt 4)',
         imageIcon: imageImports.bidWriterIcon
       },
       {
-        headerText: ui?.prompt5_header_text || 'Default Header 5',
-        suggestionText: ui?.prompt5_suggestion_text || 'Default Suggestion 5',
+        headerText: ui?.prompt5_header_text || '',
+        suggestionText: ui?.prompt5_suggestion_text || '',
         message: ui?.prompt5_suggestion_message || "What's the weather? (prompt 5)",
         imageIcon: imageImports.sendPlannerIcon
       },
       {
-        headerText: ui?.prompt6_header_text || 'Default Header 6',
-        suggestionText: ui?.prompt6_suggestion_text || 'Default Suggestion 6',
+        headerText: ui?.prompt6_header_text || '',
+        suggestionText: ui?.prompt6_suggestion_text || '',
         message: ui?.prompt6_suggestion_message || 'Give me a quote (prompt 6)',
         imageIcon: imageImports.reportWriterIcon
       },
       {
-        headerText: ui?.prompt7_header_text || 'Default Header 7',
-        suggestionText: ui?.prompt7_suggestion_text || 'Default Suggestion 7',
+        headerText: ui?.prompt7_header_text || '',
+        suggestionText: ui?.prompt7_suggestion_text || '',
         message: ui?.prompt7_suggestion_message || 'What is ML (prompt 7)?',
         imageIcon: imageImports.inspectionToolKitIcon
       },
       {
-        headerText: ui?.prompt8_header_text || 'Default Header 8',
-        suggestionText: ui?.prompt8_suggestion_text || 'Default Suggestion 8',
+        headerText: ui?.prompt8_header_text || '',
+        suggestionText: ui?.prompt8_suggestion_text || '',
         message: ui?.prompt8_suggestion_message || 'What is ML (prompt 8)?',
       }
     ];
